a046a0ad0a73abf7d86feeafc15f3050af9daa49
Unknown <m.lataster1998@gmail.com>
2017-11-06 20:58:58 +0100

    Revert "kk laravel"
    This reverts commit 000c47eb9d7864a26246364e6c0cc66befee1690.

---
+++ storage/framework/views/2aad850139bb261877cc912ab913559c18fb1ed7.php
@@ -0,0 +1,34 @@
+
+<?php $__env->startSection('content'); ?>
+
+<div class="main">
+	<div class="container">
+<?php if(!$url): ?>
+				<?php $__currentLoopData = $users; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $user): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+					<a href="<?php echo e(url('userChange',$user->id)); ?>"><?php echo e($user->name); ?></a> <br>
+				<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+<?php endif; ?>
+
+<?php if($url): ?>
+<form action="<?php echo e(route('user.update', $user->id)); ?>" method="POST">
+	Naam:<br>
+	<input type="text" name="name" value="<?php echo e($user->name); ?>"><br>
+	Achternaam: <br>
+	<input type="text" name="lastname" value="<?php echo e($user->lastname); ?>"><br>
+	Adres: <br>
+	<input type="text" name="adres" value="<?php echo e($user->adres); ?>"><br>
+
+	 <?php echo e(csrf_field()); ?>
+
+  	<?php echo e(method_field('PUT')); ?>﻿
+  	<input type="hidden" name="_method" value="PUT">
+	<input type="submit" value="Bewerk">
+</form>
+<?php endif; ?>
+
+
+	</div>
+</div>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
